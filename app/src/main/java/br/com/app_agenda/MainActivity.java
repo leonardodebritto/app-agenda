c8da4561f3ba6260a48a238e1bde47da88651157
leonardodebritto <britto.leonardo@outlook.com>
2021-06-07 15:48:46 -0300

    Provisional implementation of some textviews in the layout and their references in main

--- app/src/main/java/br/com/app_agenda/MainActivity.java
+++ app/src/main/java/br/com/app_agenda/MainActivity.java
@@ -3,9 +3,10 @@ package br.com.app_agenda;
 import android.app.Activity;
 import android.os.Bundle;
 import android.widget.TextView;
-import android.widget.Toast;
-
 import androidx.annotation.Nullable;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 public class MainActivity extends Activity {
 
@@ -13,5 +14,18 @@ public class MainActivity extends Activity {
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Simulação de uma lista de alunos - Criando uma lista com Array
+        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José"));
+
+        //Referênciando os três textviews do layout activity_main
+        TextView primeiroAluno = findViewById(R.id.textView);
+        TextView segundoAluno = findViewById(R.id.textView2);
+        TextView terceiroAluno = findViewById(R.id.textView3);
+
+        //Setando os valores de acordo com as posições do array para os textviews
+        primeiroAluno.setText(alunos.get(0));
+        segundoAluno.setText(alunos.get(1));
+        terceiroAluno.setText(alunos.get(2));
     }
 }
