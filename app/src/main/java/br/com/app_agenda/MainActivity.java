384ea5c9e0c260961c75e0ec6c395198df878d76
leonardodebritto <britto.leonardo@outlook.com>
2021-06-07 16:15:33 -0300

    creating a ListView component and creating an adapter

--- app/src/main/java/br/com/app_agenda/MainActivity.java
+++ app/src/main/java/br/com/app_agenda/MainActivity.java
@@ -2,7 +2,8 @@ package br.com.app_agenda;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import androidx.annotation.Nullable;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,16 +17,14 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
 
         //Simulação de uma lista de alunos - Criando uma lista com Array
-        List<String> alunos = new ArrayList<>(Arrays.asList("Alex", "Fran", "José"));
+        List<String> students = new ArrayList<>(Arrays.asList("Alex", "Fran", "José", "Roberto"));
 
-        //Referênciando os três textviews do layout activity_main
-        TextView primeiroAluno = findViewById(R.id.textView);
-        TextView segundoAluno = findViewById(R.id.textView2);
-        TextView terceiroAluno = findViewById(R.id.textView3);
+        //Referênciando a ListView criada no Layout
+        ListView studentList = findViewById(R.id.activity_main_student_list);
 
-        //Setando os valores de acordo com as posições do array para os textviews
-        primeiroAluno.setText(alunos.get(0));
-        segundoAluno.setText(alunos.get(1));
-        terceiroAluno.setText(alunos.get(2));
+        //Criando o adapter que irá vincular os itens da lista as respectivas views
+        //Utilizando apenas o ArrayAdapter que já implementa o ListAdapter, mas é mais simples
+        studentList.setAdapter(new ArrayAdapter<>(this,android.R.layout.simple_list_item_1, students));
+
     }
 }
