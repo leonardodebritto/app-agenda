87f0291bb1f80325fe8fac0f99b8e3965d8afabc
leonardodebritto <britto.leonardo@outlook.com>
2021-06-08 07:52:37 -0300

    Implementing the on click function in the student registration form

--- app/src/main/java/br/com/app_agenda/ui/activity/StudentFormActivity.java
+++ app/src/main/java/br/com/app_agenda/ui/activity/StudentFormActivity.java
@@ -2,7 +2,13 @@ package br.com.app_agenda.ui.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
 import br.com.app_agenda.R;
+import br.com.app_agenda.model.Student;
 
 public class StudentFormActivity extends AppCompatActivity {
 
@@ -10,5 +16,28 @@ public class StudentFormActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_student_form);
+
+        //Referênciando os Edit Text
+         final EditText nameField = findViewById(R.id.activity_student_form_name);
+         final EditText phoneField = findViewById(R.id.activity_student_form_phone);
+         final EditText emailField = findViewById(R.id.activity_student_form_email);
+
+        //Referênciando o botão salvar
+        Button saveButton = findViewById(R.id.activity_student_form_save_button);
+        //Implementando o listener para o botão
+        saveButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = nameField.getText().toString();
+                String phone = phoneField.getText().toString();
+                String email = emailField.getText().toString();
+
+                Student studentCreated = new Student(name, phone, email);
+                Toast.makeText(StudentFormActivity.this,
+                        studentCreated.getName()
+                                + " - " + studentCreated.getPhone()
+                                + " - " + studentCreated.getEmail(), Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
